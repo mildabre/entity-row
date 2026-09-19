60a1e17884f95e0646b792ac26a326f7127dbbde
milos.brecher <milos.brecher@gmail.com>
2026-09-19 21:09:06 +0200

    feat: add attribute #[SortColumn], replaces constant SortColumn

---
+++ src/Entity/SortColumn.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bite\EntityRow\Entity;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+final class SortColumn
+{
+    public function __construct(
+        public readonly string $column = 'sort',
+        public readonly int $initial = 0,
+        public readonly ?string $groupBy = null,
+    ) {}
+}
