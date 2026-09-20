62fb66224264b2979f5b176aed145900aa520d73
milos.brecher <milos.brecher@gmail.com>
2026-09-20 20:08:55 +0200

    refactor: #[Sort], rename parameter $parentEntity

--- src/Entity/Sort.php
+++ src/Entity/Sort.php
@@ -9,9 +9,12 @@ use Attribute;
 #[Attribute(Attribute::TARGET_CLASS)]
 final class Sort
 {
+    /**
+     * @param class-string<EntityRow>|null $parentEntity
+     */
     public function __construct(
         public readonly string $column = 'sort',
         public readonly int $initial = 0,
-        public readonly ?string $parentKey = null,
+        public readonly ?string $parentEntity = null,
     ) {}
 }
